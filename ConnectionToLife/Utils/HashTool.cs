0785ff8317aaae075dcdacd9aebe1b4dcd0c670e
Gameplushy <victor_f@orange.fr>
2024-01-26 14:37:39 +0100

    Hashing for passwords

---
+++ ConnectionToLife/Utils/HashTool.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+namespace ConnectionToLife.Utils
+{
+    public class HashTool
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                byte[] salt = new byte[16]; // 16 bytes for the salt
+                rng.GetBytes(salt);
+                return salt;
+            }
+        }
+
+        public static string HashPassword(string password, byte[] salt)
+        {
+            int iterations = 10000; // Number of iterations for PBKDF2
+
+            using (Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations, HashAlgorithmName.SHA256))
+            {
+                byte[] hash = pbkdf2.GetBytes(32); // 32 bytes for the hashed password
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
