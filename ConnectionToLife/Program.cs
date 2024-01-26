e33d016bdc06c53b1503b468cf45081b12cc3ed4
Gameplushy <victor_f@orange.fr>
2024-01-26 16:02:44 +0100

    Functioning login+GOL

--- ConnectionToLife/Program.cs
+++ ConnectionToLife/Program.cs
@@ -1,10 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConnectionToLife.Connection;
 using ConnectionToLife.GameOfLife;
 
-var b = new Board("23A3D");
+User user = UserConnect.ConnectionPrompt();
+
+var b = new Board(user.rules);
 b.GenerateRandomBoard();
 Console.WriteLine(b.DisplayBoard());
+Console.WriteLine($"Code règles : {user.rules}");
 Console.ReadLine();
+Console.Clear();
 Thread t = new Thread(new ThreadStart(Iterate));
 t.Start();
 Console.ReadLine();
@@ -16,12 +20,26 @@ void Iterate()
     for (int i = 0; i < 10000; i++)
     {
         GameRulesChecker.Iterate(b);
-        Console.Clear();
+        Console.SetCursorPosition(0, 0);
         Console.WriteLine(b.DisplayBoard());
         try
         {
             Thread.Sleep(500);
         }
-        catch(ThreadInterruptedException tie) { break; }
+        catch (ThreadInterruptedException tie) { break; }
     }
 }
+
+// Get user input for the password
+/*string password = "sosis";
+
+// Generate a random salt
+byte[] salt = HashTool.GenerateSalt();
+
+// Hash the password using PBKDF2
+string hashedPassword = HashTool.HashPassword(password, salt);
+
+// Display the results
+Console.WriteLine($"Original Password: {password}");
+Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+Console.WriteLine($"{hashedPassword}");*/
